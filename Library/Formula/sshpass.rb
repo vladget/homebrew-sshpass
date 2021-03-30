2c4b0f28da781221e82df5bf42a4d6eac993af18
Vladimir Getmanshchuk <vladget@gmail.com>
2021-03-30 20:26:48 +0300

    first commit

---
+++ Library/Formula/sshpass.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Sshpass < Formula
+  url 'https://deac-ams.dl.sourceforge.net/project/sshpass/sshpass/1.08/sshpass-1.08.tar.gz'
+  homepage 'http://sourceforge.net/projects/sshpass'
+  sha256 '8bdacb8ca4ecf82c84a5effa2f75a8bf4ed9dd6f9d030f308fe11cffb3830b02'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    system "sshpass"
+  end
+end
---
+++ README.md
@@ -0,0 +1,5 @@
+# homebrew-sshpass
+
+Homebrew Formula for sshpass
+
+For sshpass: `brew install https://raw.githubusercontent.com/vladget/homebrew-sshpass/master/Library/Formula/sshpass.rb`
